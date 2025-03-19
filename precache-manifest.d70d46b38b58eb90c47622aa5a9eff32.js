5b19a252e30273ed0158fab0c25ae513cc8d3709
Aswin0056 <aswinayyappan056@gmail.com>
2025-03-19 13:36:30 +0530

    Updates

---
+++ precache-manifest.d70d46b38b58eb90c47622aa5a9eff32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c33a96c63f7a0bba3004eea8c365f99d",
+    "url": "/ExpenSaver/index.html"
+  },
+  {
+    "revision": "6bacd3ea2286efd5855e",
+    "url": "/ExpenSaver/static/css/main.8744448a.chunk.css"
+  },
+  {
+    "revision": "2b6c36f733eecfb0130e",
+    "url": "/ExpenSaver/static/js/2.7b906718.chunk.js"
+  },
+  {
+    "revision": "83c6f0a82f430d893ae6c1998b3a1c17",
+    "url": "/ExpenSaver/static/js/2.7b906718.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bacd3ea2286efd5855e",
+    "url": "/ExpenSaver/static/js/main.b964916e.chunk.js"
+  },
+  {
+    "revision": "17b7044697f8c3f08b62",
+    "url": "/ExpenSaver/static/js/runtime-main.e3edf58e.js"
+  }
+]);
