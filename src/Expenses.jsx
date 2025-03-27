124621927f72028360bf8db16eedbf076994a530
Aswin0056 <aswinayyappan056@gmail.com>
2025-03-27 23:15:21 +0530

    updated-designer

--- src/Expenses.jsx
+++ src/Expenses.jsx
@@ -240,6 +240,7 @@ const Expenses = () => {
                     <a href="https://in.linkedin.com/in/aswin-i-1543b0259?trk=people-guest_people_search-card" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
                   </div>
                 </div>
+                <p className="designed">Designed by <strong className="cursive">Lishoni Lenin</strong></p>
               </div>
             </footer>
     </div>
